109b0d8a30549830fe678b1414125083d44c0c08
maksimcialehin <madqfrog1863@gmail.com>
2022-11-03 11:16:42 +0300

    add login and registration check

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from pages.base_page import BasePage
+from pages.locators import MainPageLocators, LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, 'No "login" in current url'
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
---
+++ pages/main_page.py
@@ -0,0 +1,11 @@
+from pages.base_page import BasePage
+from pages.locators import MainPageLocators
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
